48ab1428519a4d7953c46bec891bfce4a79b246a
Gbeltrame12 <guidoagustinbeltrame@gmail.com>
2017-08-21 13:44:07 -0300

    Creando metodo getResultado de indicador

--- src/main/java/ar/indicadores/Indicador.java
+++ src/main/java/ar/indicadores/Indicador.java
@@ -1,11 +1,19 @@
 package ar.indicadores;
 
+import java.io.IOException;
+
+import org.antlr.v4.runtime.ANTLRInputStream;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.tree.ParseTree;
+
+import ar.indicadores.antlr.VisitorExp;
+
+@SuppressWarnings("deprecation")
 public class Indicador implements IExpresion {
 
 	private String nombre;
 	private String formula;
-	private double valor;
-	
+
 	public String getNombre() {
 		return nombre;
 	}
@@ -22,15 +30,36 @@ public class Indicador implements IExpresion {
 		this.formula = formula;
 	}
 
-	public double getValor() {
-		return valor;
-	}
+	public double getResultado() {
 
-	public void setValor(double valor) {
-		this.valor = valor;
+		// Tomo el dato de la formula para calcular
+		String form = this.formula;
+
+		//Creo el lexer a partir de la formula
+		IndicadoresLexer lexer = new IndicadoresLexer(new ANTLRInputStream(form));
+		
+		//Creo los tokens a partir del lexer
+		CommonTokenStream tokens = new CommonTokenStream(lexer);
+		
+		//Creo el parser a partir de los tokens
+		IndicadoresParser parser = new IndicadoresParser(tokens);
+		
+		//Creo el arbol para el indicador
+		ParseTree tree = parser.indicador();
+		
+		//Creo el visitor para recorrer
+		VisitorExp visitor = new VisitorExp();
+		
+		//Obtengo la expresion a partir del visitor 
+		IExpresion expresion = visitor.visit(tree);
+
+		return expresion.getResultado();
+
 	}
 	
-	public double getResultado() {
-		return valor;
+	public void validarFormula() throws Exception {
+		
+		
+		
 	}
 }
